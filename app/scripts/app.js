9dd68977bfc318ffd223282f73097f4882aa8417
jessecravens <jesse.cravens@gmail.com>
2014-04-16 23:44:47 -0500

    Chapter 4: Add custom Handlebars helper (hotttnesss-badge) and add helper syntax to index.hbs template.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,19 +7,22 @@ RocknrollcallYeoman.dummySearchResultsArtists = [
     id: 1,
     name: 'Tom Waits',
     type: 'artist',
-    enid: 'ARERLPG1187FB3BB39'
+    enid: 'ARERLPG1187FB3BB39',
+    hotttnesss: '1'
   },
   {
     id: 2,
     name: 'Thomas Alan Waits',
     type: 'artist',
-    enid: 'ARERLPG1187FB3BB39'
+    enid: 'ARERLPG1187FB3BB39',
+    hotttnesss: '.89'
   },
   {
     id: 3,
     name: 'Tom Waits w/ Keith Richards',
     type: 'artist',
-    enid: 'ARMPVNN13CA39CF8FC'
+    enid: 'ARMPVNN13CA39CF8FC',
+    hotttnesss: '.79'
   }
 ];
 
@@ -30,3 +33,21 @@ require('scripts/models/*');
 require('scripts/routes/*');
 require('scripts/views/*');
 require('scripts/router');
+
+Ember.Handlebars.helper('hotttnesss-badge', function(value, options) {
+  var h = parseFloat(value);
+  var hotttnesss_num = Math.round(h * 100);
+  var hotttnesss_css = Math.ceil(h * 10) - 1;
+  var html = "<h4>Hotness: ";
+  if (hotttnesss_num > 0) {
+    html += '<i class="hotttnesss">';
+    for (var i=0;i<hotttnesss_css;i++) {
+      html += '<i class="glyphicon glyphicon-fire hotttnesss'+i+'"></i>';
+    }
+    html += "</i>";
+    html += '<span class="hotttnesss-badge">'+hotttnesss_num+'</span></h4>';
+  } else {
+    html += "0</h4>";
+  }
+  return new Handlebars.SafeString(html);
+});
