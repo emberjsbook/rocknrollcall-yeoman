54bad21dcf0f349454607ce23513aa1e8b4df9a2
jessecravens <jesse.cravens@gmail.com>
2014-04-16 22:16:03 -0500

    Chapter 4: Add global RocknrollcallYeoman.dummySearchResultsArtists

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,6 +2,28 @@ var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create(
 
 RocknrollcallYeoman.applicationName = "Rock'n'Roll Call";
 
+RocknrollcallYeoman.dummySearchResultsArtists = [
+  {
+    id: 1,
+    name: 'Tom Waits',
+    nickname: 'Tommy',
+    type: 'artist',
+    enid: 'ARERLPG1187FB3BB39'
+  },
+  {
+    id: 2,
+    name: 'Thomas Alan Waits',
+    type: 'artist',
+    enid: 'ARERLPG1187FB3BB39'
+  },
+  {
+    id: 3,
+    name: 'Tom Waits w/ Keith Richards',
+    type: 'artist',
+    enid: 'ARMPVNN13CA39CF8FC'
+  }
+];
+
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
