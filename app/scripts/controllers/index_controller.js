30ea38c01997c4aa27b3c0a27d1ae0db4cbedbf2
jessecravens <jesse.cravens@gmail.com>
2014-04-16 23:25:25 -0500

    Chapter 4: Add actions in the indextemplate, and actions handler (viewedArtist) in the IndexController for demonstration purposes (will be removed later).

--- app/scripts/controllers/index_controller.js
+++ app/scripts/controllers/index_controller.js
@@ -1,2 +1,7 @@
 RocknrollcallYeoman.IndexController = Ember.Controller.extend({
+  actions: {
+    viewedArtist: function(artist) {
+      console.log('hang on Im viewing: ' + artist.name)
+    }
+  }
 });
