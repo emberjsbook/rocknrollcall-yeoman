4917579356709e88e7356fa09c99df57b7ee7ac2
jessecravens <jesse.cravens@gmail.com>
2014-04-17 22:50:55 -0500

    Chapter 5: Remove RocknrollcallYeoman.dummySearchResultsArtists.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,30 +5,6 @@ var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create(
 
 RocknrollcallYeoman.applicationName = "Rock'n'Roll Call";
 
-RocknrollcallYeoman.dummySearchResultsArtists = [
-  {
-    id: 1,
-    name: 'Tom Waits',
-    type: 'artist',
-    enid: 'ARERLPG1187FB3BB39',
-    hotttnesss: '1'
-  },
-  {
-    id: 2,
-    name: 'Thomas Alan Waits',
-    type: 'artist',
-    enid: 'ARERLPG1187FB3BB39',
-    hotttnesss: '.89'
-  },
-  {
-    id: 3,
-    name: 'Tom Waits w/ Keith Richards',
-    type: 'artist',
-    enid: 'ARMPVNN13CA39CF8FC',
-    hotttnesss: '.79'
-  }
-];
-
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
