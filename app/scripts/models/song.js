259fefe49f4b2afe6593e2dbfb29a35dfacb6f0b
jessecravens <jesse.cravens@gmail.com>
2014-04-18 00:05:16 -0500

    Chapter 6: Add in SongObject.

---
+++ app/scripts/models/song.js
@@ -0,0 +1,12 @@
+RocknrollcallYeoman.Song = Em.Object.extend({
+  id: null,
+  enid: null,
+  title: null,
+  artist_name: null,
+  artist_id: null,
+  audio_md5: null,
+  audio_summary: null,
+  hotttnesss: null,
+  track: null,
+  types: null
+});
