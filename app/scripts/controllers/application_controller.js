6e86ecba5c458b8af227e2cb37d62c3182caad02
jessecravens <jesse.cravens@gmail.com>
2014-04-18 00:03:37 -0500

    Chapter 6: Bind ApplicationController’s applicationName with a computed property that observes changes to searchTerms.

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -1,6 +1,13 @@
 RocknrollcallYeoman.ApplicationController = Em.ObjectController.extend({
   searchTerms: '',
-  applicationName: "Rock'n'Roll Call",
+  applicationName: function() {
+    var st = this.get('searchTerms');
+    if (st) {
+      return st + "???"
+    } else {
+      return "Rock'n'Roll Call"
+    }
+  }.property('searchTerms'),
   actions: {
     submit: function() {
       this.transitionToRoute('search-results',this.get('searchTerms'));
