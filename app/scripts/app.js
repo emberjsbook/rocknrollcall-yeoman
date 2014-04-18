78d25248128aa5fb9b4a1296be1bf741b00d3652
jessecravens <jesse.cravens@gmail.com>
2014-04-17 22:47:23 -0500

    Chapter 5: Add LOG_ACTIVE_GENERATION to the Ember.Application object.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,6 @@
 var RocknrollcallYeoman = window.RocknrollcallYeoman = Ember.Application.create({
-  LOG_TRANSITIONS: true
+  LOG_TRANSITIONS: true,
+  LOG_ACTIVE_GENERATION: true
 });
 
 RocknrollcallYeoman.applicationName = "Rock'n'Roll Call";
