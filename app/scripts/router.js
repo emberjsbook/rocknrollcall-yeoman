da720f25a0197a2a0935490bbde9688a33ba4c67
jessecravens <jesse.cravens@gmail.com>
2014-04-17 23:01:09 -0500

    Chapter 5: Add in dynamic segment (:enid) to ArtistRoute and SongRoute.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -2,6 +2,10 @@ RocknrollcallYeoman.Router.map(function() {
   this.route('search-results', {
     path: 'search/:term'
   });
-  this.route('artist');
-  this.route('song');
+  this.route('artist', {
+    path: 'artist/:enid'
+  });
+  this.route('song', {
+    path: 'song/:enid'
+  });
 });
