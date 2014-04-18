b80a3d1c68b8cc4b39b288bf2d40daa3b5ce16a0
jessecravens <jesse.cravens@gmail.com>
2014-04-18 00:54:08 -0500

    Chapter 6: Use SongView’s didInsertElement to convert seconds to "hh:mm:ss".

--- app/scripts/views/song_view.js
+++ app/scripts/views/song_view.js
@@ -1,1 +1,11 @@
-RocknrollcallYeoman.SongView = Em.View.extend({});
+RocknrollcallYeoman.SongView = Em.View.extend({
+  didInsertElement: function() {
+    $('#song-duration').text(function(){
+      var $this = $(this);
+      var origSeconds = $this.attr('data-duration');
+      var minutes = Math.floor(origSeconds/60);
+      var seconds = Math.floor(origSeconds % 60);
+      return minutes + ":" + seconds
+    });
+  }
+});
