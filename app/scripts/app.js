abb125499961bd61f21d62050f9f3418eb484d87
jessecravens <jesse.cravens@gmail.com>
2014-04-21 15:44:21 -0500

    Chapter 7: Add an Application Initializer to seed the Ember Data Store with 300 activities.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,3 +28,54 @@ Ember.Handlebars.helper('hotttnesss-badge', function(value, options) {
   }
   return new Handlebars.SafeString(html);
 });
+
+Ember.Application.initializer({
+  name: "DBseeds",
+  initialize: function(container, application) {
+
+    localStorage.clear();
+
+    store = container.lookup('store:main');
+    console.log('store: ', store);
+
+    for (var i = 0; i < 300; i++) {
+
+      var id = 'ARNH6LU1187FB430FA';
+      var random_id = id.split('').sort(function() {
+        return 0.5 - Math.random()
+      }).join('');
+
+      var types = [
+        'song',
+        'artist'
+      ];
+
+      var random_type = Math.floor(Math.random() * types.length);
+
+      var name = 'Jesse Cravens';
+      var random_name = name.split('').sort(function() {
+        return 0.5 - Math.random()
+      }).join('');
+
+      var random_hotness = Math.floor(Math.random() * 100) + 1;
+       var random_timestamp = new Date(new Date(2013, 9, 30).getTime() + Math.random() *
+        (new Date().getTime() - new Date(2013, 9, 30).getTime()));
+
+      activity = store.createRecord('activity', {
+        display_id: random_id,
+        type: types[random_type],
+        display_name: random_name,
+        hotttnesss: random_hotness,
+        timestamp: random_timestamp
+      });
+
+      activity.save();
+    };
+
+    console.log(store.find('activity')
+      .then(function(stuff) {
+      console.log('Total Activity Records: ' + stuff.toArray().length)
+      })
+    );
+  }
+});
