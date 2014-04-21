7ff45ad60b7e80e6387443dccf37d4a9d0d61b90
jessecravens <jesse.cravens@gmail.com>
2014-04-21 10:30:56 -0500

    Chapter 7: Add fixture data for the Activity model.

--- app/scripts/models/activity.js
+++ app/scripts/models/activity.js
@@ -5,3 +5,19 @@ RocknrollcallYeoman.Activity = DS.Model.extend({
   hotttnesss: DS.attr('number'),
   timestamp: DS.attr()
 });
+
+RocknrollcallYeoman.Activity.FIXTURES = [{
+  id: 0,
+  display_id: "Activity1",
+  type: "song",
+  display_name: "On the Road Again",
+  hotttnesss: 54,
+  timestamp: "Fri Dec 06 2013 01:05:53 GMT-0600 (CST)"
+}, {
+  id: 1,
+  display_id: "Activity2",
+  type: "artist",
+  display_name: "Willie Nelson",
+  hotttnesss: 99,
+  timestamp: "Fri Dec 06 2013 01:05:53 GMT-0600 (CST)"
+}]
