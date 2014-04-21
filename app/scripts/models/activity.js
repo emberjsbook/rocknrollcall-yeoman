35fb871aa5aa4b95f524db0b0fdc2c3eb33e1867
jessecravens <jesse.cravens@gmail.com>
2014-04-21 10:30:16 -0500

    Chapter 7: Add the Activity model.

---
+++ app/scripts/models/activity.js
@@ -0,0 +1,7 @@
+RocknrollcallYeoman.Activity = DS.Model.extend({
+  display_id: DS.attr('string'),
+  type: DS.attr('string'),
+  display_name: DS.attr('string'),
+  hotttnesss: DS.attr('number'),
+  timestamp: DS.attr()
+});
