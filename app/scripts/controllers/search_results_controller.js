a2d99f48bec1a314f7cd9a2f7a8178b475c3dcbf
jessecravens <jesse.cravens@gmail.com>
2014-04-21 12:56:37 -0500

    Chapter 7: Update SearchResultsController to include viewedArtist and viewedSong actions.

--- app/scripts/controllers/search_results_controller.js
+++ app/scripts/controllers/search_results_controller.js
@@ -1,4 +1,38 @@
 RocknrollcallYeoman.SearchResultsController = Em.ObjectController.extend({
   artistsIsChecked: true,
-  songsIsChecked: true
+  songsIsChecked: true,
+  actions: {
+
+    viewedArtist: function(model) {
+
+      var date = Date.now();
+
+      var activity = this.store.createRecord('activity', {
+        display_id: model.enid,
+        type: model.type,
+        display_name: model.name,
+        hotttnesss: model.hotttnesss,
+        timestamp: date
+      });
+
+      activity.save();
+
+    },
+
+    viewedSong: function(model) {
+
+      var date = Date.now();
+
+      var activity = this.store.createRecord('activity', {
+        display_id: model.enid,
+        type: model.type,
+        display_name: model.artist_name,
+        hotttnesss: model.hotttnesss,
+        timestamp: date
+      });
+
+      activity.save();
+
+    }
+  }
 });
